8fc8b61e0b87e78892e46dcf03ae1fc0ea30f294
593296 <labibfawaz@c02v21fghtdg.users.bah.com>
2017-10-10 17:15:02 -0400

    register model to admin

--- myDjangoStuff/simplesocial/simplesocial/groups/admin.py
+++ myDjangoStuff/simplesocial/simplesocial/groups/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from groups.models import Group, GroupMember
 # Register your models here.
+
+class GroupMemberInLine(admin.TabularInline):
+    model = GroupMember
+
+admin.site.register(Group)
